7d3b5c52dc58b8ba9a29cd83099190fbc37b3204
Sawyer Burnett <sawyerburnett@gmail.com>
2018-12-30 06:17:48 -0700

    remove /login route from auth because it is already matched in server.js

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,12 +1,7 @@
 const auth = require("express").Router();
 
-auth.get("/login", (req, res) => {
+auth.get("/", (req, res) => {
   res.send("hitting the route!");
 });
 
 module.exports = auth;
-
-//todo move authentication code to this section
-// export default function checkIfAuthenticated() {
-//   console.log("testing");
-// }
