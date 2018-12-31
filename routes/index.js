8f94dea62da7ee50ee552daa7671e6ef14dac428
Sawyer Burnett <sawyerburnett@gmail.com>
2018-12-31 14:05:55 -0700

    got basic auth working ... very messy

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,18 @@
 const router = require("express").Router();
 const mock = false;
 const axios = require("axios");
-
+let token = "";
 //todo create a consts file?
 const authorizeUrl = `https://www.strava.com/oauth/authorize?client_id=${
   process.env.CLIENT_ID
-}&response_type=code&redirect_uri=http://127.0.0.1:3000&scope=activity:read_all&approval_prompt=force`;
+}&response_type=code&redirect_uri=http://127.0.0.1:3000&scope=read_all,activity:read_all&approval_prompt=auto`;
 
 const initialCall = `https://www.strava.com/api/v3/athletes/${
   process.env.ATHLETE_ID
-}?access_token=${process.env.TOKEN}`;
+}`;
 
 let config = {
   headers: { accept: "application/json" }
-};
-
-let bearer = {
-  headers: { authorization: "Bearer " }
 };
 
 const stats = {
@@ -76,49 +72,94 @@ router.get("/", (req, res) => {
     process.env.CLIENT_ID
   }&response_type=code&redirect_uri=http://127.0.0.1:3000&scope=activity:read_all&approval_prompt=force`;
   console.log(`calling ${authorizeUrl}`);
+  console.log(`token ${token}`);
 
-  if (checkIfAuthenticated()) {
-    axios
-      .get(initialCall)
-      .then(function(response) {
-        //console.log(response);
-        res.send(response.data);
-      })
-      .catch(function(err) {
-        console.log(err.message);
-      });
-  }
+  // if (checkIfAuthenticated()) {
+  //   axios
+  //     .get(initialCall)
+  //     .then(function(response) {
+  //       //console.log(response);
+  //       res.json(response.data);
+  //     })
+  //     .catch(function(err) {
+  //       res.json(err);
+  //     });
+  // }
 
   if (checkIfAuthenticated()) {
     res.send(
       "we are now authenticated! localhost:3000/?code=kafdk234234ksdfk234k234"
     );
   } else if (req.query.code) {
-    authenticated = true;
-    token = req.query.code;
+    let authCode = req.query.code;
 
-    res.json({ status: "authenticated!", token });
+    let url = `https://www.strava.com/oauth/token?client_id=${
+      process.env.CLIENT_ID
+    }&client_secret=${
+      process.env.CLIENT_SECRET
+    }&code=${authCode}&grant_type=authorization_code`;
+
+    console.log(`got code, now calling ${url}`);
+
+    axios
+      .post(url)
+      .then(function(response) {
+        authenticated = true;
+        console.log("we are authenticated!");
+        token = response.data.access_token;
+      })
+      .catch(function(err) {
+        res.send(err);
+      });
+
+    res.json({ status: "code from redirect!", authCode });
   } else {
     res.json({ authenticate: authorizeUrl });
   }
 });
 
+router.get("/athlete", (req, res) => {
+  let athleteUrl = "https://www.strava.com/api/v3/athlete";
+
+  let bearer = {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+
+  axios
+    .get(athleteUrl, bearer)
+    .then(function(response) {
+      res.json(response.data);
+    })
+    .catch(function(err) {
+      //console.log(err);
+      res.json(err.message);
+    });
+});
+
 router.get("/stats", (req, res) => {
+  if (!token) {
+    res.send("we are not authenticated yet");
+  }
+
+  let bearer = {
+    headers: { authorization: "Bearer ${token}" }
+  };
+
   let statsCall = `https://www.strava.com/api/v3/athletes/${
     process.env.ATHLETE_ID
-  }/stats/?access_token=${process.env.TOKEN}`;
+  }/stats`;
   console.log(`calling ${statsCall}`);
   if (mock) {
     res.json(stats);
   } else {
     axios
-      .get(statsCall, {}, config)
+      .get(statsCall, bearer)
       .then(function(response) {
         console.log(response);
         res.json(response.data);
       })
       .catch(function(err) {
-        console.log(err);
+        res.send(err);
       });
   }
 });
@@ -128,7 +169,7 @@ router.get("/activities", (req, res) => {
   const activities2Call = `https://www.strava.com/api/v3/athlete/activities?per_page=30?access_token=${token}`;
 
   axios
-    .get(activities2Call, {}, config)
+    .get(activities2Call, config)
     .then(function(response) {
       console.log(response);
       res.json(response.data);
@@ -140,8 +181,9 @@ router.get("/activities", (req, res) => {
 
 function checkIfAuthenticated() {
   //see if we are authenticated
-  console.log(`authenticated : ${mock}`);
   if (mock) {
+    return true;
+  } else if (token) {
     return true;
   } else {
     return false;
