645d3621df8170594072dda184633948f16d6001
Sawyer Burnett <sawyerburnett@gmail.com>
2019-01-05 14:25:46 -0700

    clean up server, add 404 error code for not found routes

--- server.js
+++ server.js
@@ -6,10 +6,18 @@ const port = process.env.PORT || 3000;
 
 //run dotenv package to pull in important variables from .env file
 require("dotenv").config();
-
-//https://www.strava.com/oauth/authorize?client_id=31073&response_type=code&redirect_uri=http://strava.com&approval_prompt=force
-
 app.use("/", routes);
 app.use("/login", auth);
+
+//adding in 404 error as a fallback if no other routes are hit
+app.get("*", function(req, res) {
+  res.status(404);
+  if (req.accepts("json")) {
+    res.send({ error: "Not found" });
+    return;
+  }
+
+  res.type("txt").send("Not found");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
