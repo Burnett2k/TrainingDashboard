e73eaec2ebc896caf3e4714761e079e962b008dd
Sawyer Burnett <sawyerburnett@gmail.com>
2019-01-07 06:31:38 -0700

    move authentication code into server temporarily so that it can be ran upon startup rather than when we hit a route.  Next step will be to move it into a separate controller

--- server.js
+++ server.js
@@ -2,12 +2,36 @@ const express = require("express");
 const app = express();
 const routes = require("./routes/index");
 const auth = require("./routes/auth");
+const axios = require("axios");
 const port = process.env.PORT || 3000;
 
 //run dotenv package to pull in important variables from .env file
 require("dotenv").config();
+
+//add in routes
 app.use("/", routes);
 app.use("/login", auth);
+
+authenticate();
+
+function authenticate() {
+  let url = `https://www.strava.com/oauth/token?client_id=${
+    process.env.CLIENT_ID
+  }&client_secret=${process.env.CLIENT_SECRET}&code=${
+    process.env.RESPONSE_CODE
+  }&grant_type=authorization_code`;
+
+  console.log(`got code, now calling ${url}`);
+
+  axios
+    .post(url)
+    .then(function(response) {
+      authenticated = true;
+      console.log("we are authenticated!");
+      global.token = response.data.access_token;
+    })
+    .catch(function(err) {});
+}
 
 //adding in 404 error as a fallback if no other routes are hit
 app.get("*", function(req, res) {
