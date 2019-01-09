f2be4329d9c081eeaeed8ecbf1b0382fd83fd355
Sawyer Burnett <sawyerburnett@gmail.com>
2019-01-08 20:30:42 -0700

    only authenticate if environment variables are supplied for dev mode

--- server.js
+++ server.js
@@ -15,26 +15,34 @@ app.use("/login", auth);
 authenticate();
 
 function authenticate() {
-  let url = `https://www.strava.com/oauth/token?client_id=${
-    process.env.CLIENT_ID
-  }&client_secret=${process.env.CLIENT_SECRET}&code=${
+  if (
+    process.env.CLIENT_ID &&
+    process.env.CLIENT_SECRET &&
     process.env.RESPONSE_CODE
-  }&grant_type=authorization_code`;
+  ) {
+    let url = `https://www.strava.com/oauth/token?client_id=${
+      process.env.CLIENT_ID
+    }&client_secret=${process.env.CLIENT_SECRET}&code=${
+      process.env.RESPONSE_CODE
+    }&grant_type=authorization_code`;
 
-  console.log(`got code, now calling ${url}`);
+    console.log(`got code, now calling ${url}`);
 
-  axios
-    .post(url)
-    .then(function(response) {
-      authenticated = true;
-      console.log(
-        `we are authenticated! token = ${response.data.access_token}`
-      );
+    axios
+      .post(url)
+      .then(function(response) {
+        authenticated = true;
+        console.log(
+          `we are authenticated! token = ${response.data.access_token}`
+        );
 
-      //Here we are setting a global variable for the token value so that it can be used for Strava Api requests in any file
-      global.token = response.data.access_token;
-    })
-    .catch(function(err) {});
+        //Here we are setting a global variable for the token value so that it can be used for Strava Api requests in any file
+        global.token = response.data.access_token;
+      })
+      .catch(function(err) {});
+  } else {
+    console.log("We cannot authenticate due to a missing environment variable");
+  }
 }
 
 //adding in 404 error as a fallback if no other routes are hit
