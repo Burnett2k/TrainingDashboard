fbbf0d1363affe6d4bffe3061a8243fb4dbd36c0
Sawyer Burnett <sawyerburnett@gmail.com>
2019-01-12 14:50:14 -0700

    move server stuff into server folder
    add auth.js file to handle authentication piece of project
    
    add a dashboard link to the front page

--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-const auth = require("express").Router();
-
-auth.get("/", (req, res) => {
-  res.send("hitting the route!");
-});
-
-module.exports = auth;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const routes = require("../routes/index");
+const auth = require("./auth");
+const port = process.env.PORT || 3000;
+
+//run dotenv package to pull in important variables from .env file
+require("dotenv").config();
+
+//add in routes
+app.use("/", routes);
+
+auth.authenticate();
+
+function generateWeeksOfYear() {
+  //goal: generate a list of sums of miles ridden separated by week.
+  //Ex: week 1 (1/1 - 1/6) : 22
+  //Ex: week 2 (1/7 - 1/13) : 39
+  //2 options, we could loop through and create a list of each week and do a HTTP call
+  //for each, or we could get the data for the whole year with a single http call
+  // and then loop through all of it to summarize it.
+  //the second option would require fewer network requests, so it is probably for the best
+  //todo, HTTP call to get data for the entire year, Loop through the data and summarize each entry into what week it fits in.
+}
+
+//adding in 404 error as a fallback if no other routes are hit
+app.get("*", function(req, res) {
+  res.status(404);
+  if (req.accepts("json")) {
+    res.send({ error: "Not found" });
+    return;
+  }
+
+  res.type("txt").send("Not found");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
